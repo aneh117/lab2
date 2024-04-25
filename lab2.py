9bc6cf4f901f10608963250df06941c404d01cf8
ani <anirudh77.23@ichat.sp.edu.sg>
2024-04-25 16:57:30 +0800

    enter

--- lab2.py
+++ lab2.py
@@ -1,1 +1,36 @@
-print ("ET0735 (Devops for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("display_main_menu")
+
+def get_user_input():
+    
+    user=input('enter an input integer').split(",")
+    print (user) 
+    #convert str list to float
+    listValues = list(map(float,user))
+    return listValues
+ 
+def main():
+    display_main_menu()
+    numlist=get_user_input()
+    calc_average_temperature(numlist)
+    
+
+
+def calc_average_temperature(numlist):
+    print("calc_average")
+    total=sum(numlist)
+    avg = total/len(numlist)
+    print(avg)
+    return avg
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
