e5b5c8af416381b64499bea78707295c883531cc
ani <anirudh77.23@ichat.sp.edu.sg>
2024-04-25 15:41:09 +0800

    enter

--- bmi.py
+++ bmi.py
@@ -1,4 +1,9 @@
 def calculate_bmi(height,weight):
-    print("Height="+height)
-    print("Weight="+weight)
-calculate_bmi(weight="57",height="1.73")
+    print("Height="+str(height))
+    print("Weight="+str(weight))
+
+    bmi=weight/(height**2)
+
+    print ("bmi = "+str(bmi))
+
+calculate_bmi(1.73,57)
